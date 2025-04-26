cb9638ba5cf37e6b0a7deff063e4246b97c208ce
Nathan <nathan.leblanc171@gmail.com>
2025-04-25 21:29:06 -0400

    Add ClientStorage base project

---
+++ ClientStorage/ClientStorage/Controllers/ClientController.cs
@@ -0,0 +1,34 @@
+﻿using ClientStorage.Database;
+using ClientStorage.Database.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClientStorage.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ClientController : ControllerBase
+    {
+        private readonly ClientRepository _clientRepo;
+
+        public ClientController(ClientRepository client)
+        {
+            _clientRepo = client;
+        }
+
+        //TODO: Think about adding a mediator for logic check of valid inputs
+
+        [HttpPost]
+        public async Task<IActionResult> AddClient([FromBody] Client client)
+        {
+            await _clientRepo.AddClientAsync(client);
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetClients()
+        {
+            var clients = await _clientRepo.GetClientsAsync();
+            return Ok(clients);
+        }
+    }
+}
---
+++ ClientStorage/ClientStorage/Program.cs
@@ -0,0 +1,32 @@
+﻿using ClientStorage.Database;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder();
+
+// Add services to the container
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//Register DbContext with PostgresSQL
+builder.Services.AddDbContext<ClientDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+//Register classes and interfaces
+builder.Services.AddScoped<ClientRepository>();
+
+var app = builder.Build();
+
+// Middleware
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
+
