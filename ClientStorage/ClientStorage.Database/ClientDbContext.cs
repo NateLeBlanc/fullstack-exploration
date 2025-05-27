4b1504169e86073283da5d5e23831603043d7159
Nathan LeBlanc <nathan.leblanc@cognex.com>
2025-05-27 14:43:12 -0400

    Add OnModelCreating to DbContext

--- ClientStorage/ClientStorage.Database/ClientDbContext.cs
+++ ClientStorage/ClientStorage.Database/ClientDbContext.cs
@@ -1,13 +1,19 @@
 ﻿using ClientStorage.Database.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace ClientStorage.Database
-{
-    public class ClientDbContext : DbContext
-    {
-        public ClientDbContext(DbContextOptions<ClientDbContext> options)
-            : base(options) { }
+namespace ClientStorage.Database;
 
-        public DbSet<Client> Clients { get; set; }
+public class ClientDbContext : DbContext
+{
+    public DbSet<Client> Clients { get; set; }
+
+    public ClientDbContext(DbContextOptions<ClientDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<Client>()
+            .HasKey(client => client.Id);
     }
 }
