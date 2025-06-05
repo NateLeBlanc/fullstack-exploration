fa82aef59b91700ddd9442576152d423e0ad57a0
Nathan LeBlanc <nathan.leblanc@cognex.com>
2025-06-05 13:04:03 -0400

    Add first and last name to Client

--- ClientStorage/ClientStorage.Database/Models/Client.cs
+++ ClientStorage/ClientStorage.Database/Models/Client.cs
@@ -3,6 +3,7 @@
 public class Client
 {
     public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
     public int Age { get; set; }
 }
